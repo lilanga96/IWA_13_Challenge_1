93de5e701f3690a423952eedcaa38105bca2d729
Siphesihle <Siphesihledyalvane@gmail.com>
2023-04-19 21:44:45 +0200

    debugged the code

---
+++ scripts.js
@@ -0,0 +1,56 @@
+let state = 'idle'
+let user = null
+let calculated = 1
+
+// Only allowed to change below
+
+
+function logCalc() {
+    const isString = calculated;
+    const calculatedAsNumber = calculated;
+    const calculated = calculated + 1
+
+
+}
+
+
+
+
+
+
+
+const calcUser = () => {
+
+    if (calculated = 1) { user = 'John' }
+    if (calculated = 1) { state = 'requesting' }
+    if (calculated = 1) { state = 'idle' }
+}
+
+const checkUser = () => {
+
+    if (user === 'John' && state === 'requesting') {
+
+        console.log(`User: ${user} (${calculated})`)
+    }
+}
+console.log(`user: ${user} (${calculated + 1})`)
+
+
+// Only allowed to change code above
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+checkUser()
+calcUser()
+
+
